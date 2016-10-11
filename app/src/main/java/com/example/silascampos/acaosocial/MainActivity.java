1a87eda84b467ebafadebe5f051e1a84cc3c9377
Nemo-PC-25\Silas Campos <silas_campos_23@hotmail.com>
2016-10-11 13:26:52 -0300

    11/10

--- app/src/main/java/com/example/silascampos/acaosocial/MainActivity.java
+++ app/src/main/java/com/example/silascampos/acaosocial/MainActivity.java
@@ -53,6 +53,40 @@ public class MainActivity extends AppCompatActivity {
                 popup.showAsDropDown(button1, 0, 0);
             }});
 
+        //Configure Markers and POPUP on button 2
+        final Instituicao button2 = (Instituicao)findViewById(R.id.button2);
+        button2.setNome("Instituição 2");
+
+        button2.setOnClickListener(new PopupClickListener(popup){
+
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater
+                        = (LayoutInflater)getBaseContext()
+                        .getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = layoutInflater.inflate(R.layout.activity_popup_, null);
+                if(popup!=null){popup.dismiss();popup=null;}
+                popup = new PopupWindow(
+                        popupView,
+                        ActionBar.LayoutParams.WRAP_CONTENT,
+                        ActionBar.LayoutParams.WRAP_CONTENT);
+
+                //Popup Button Click
+                Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+                btnDismiss.setOnClickListener(new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        // TODO Auto-generated method stub
+                        popup.dismiss();
+                    }});
+
+                //Set Text to correspondent i
+                TextView txt = (TextView)popupView.findViewById(R.id.popup_nome);
+                txt.setText(button2.getNome());
+
+                popup.showAsDropDown(button2, 0, 0);
+            }});
+
         //Map Click
         RelativeLayout app_layer = (RelativeLayout) findViewById (R.id.Map);
         app_layer.setOnClickListener(new View.OnClickListener() {
