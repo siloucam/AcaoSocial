122ea0c6515be579b6190b57b57d6432770b6a4f
Nemo-PC-25\Silas Campos <silas_campos_23@hotmail.com>
2016-10-17 14:36:03 -0200

    17/10

--- app/src/main/java/com/example/silascampos/acaosocial/AgendarVisita.java
+++ app/src/main/java/com/example/silascampos/acaosocial/AgendarVisita.java
@@ -6,15 +6,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
 
 @SuppressWarnings("deprecation")
 public class AgendarVisita extends AppCompatActivity{
+    CalendarView calendarView;
+    TextView dateDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agendar_visita);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        dateDisplay = (TextView) findViewById(R.id.date_display);
+        dateDisplay.setText("Date: ");
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                dateDisplay.setText("Date: " + i2 + " / " + i1 + " / " + i);
+            }
+        });
+
 
     }
 }
