92c71196c9664cc5ab15c24fe9e81d0b739f99c4
siloucam <silas_campos_23@hotmail.com>
2016-11-23 11:12:41 -0200

    23/11

---
+++ app/src/main/java/com/example/silascampos/acaosocial/View/Login.java
@@ -0,0 +1,35 @@
+package com.example.silascampos.acaosocial.View;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.example.silascampos.acaosocial.R;
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/silascampos/acaosocial/View/MapsActivity.java
+++ app/src/main/java/com/example/silascampos/acaosocial/View/MapsActivity.java
@@ -5,15 +5,19 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.silascampos.acaosocial.R;
+/*
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+*/
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+//public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity{
 
+    /*
     private GoogleMap mMap;
 
     @Override
@@ -27,15 +31,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Log.d("myTag", String.valueOf("Map Ready!"));
@@ -51,4 +47,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.clear();
         Log.d("myTag", String.valueOf("Map Ready End!"));
     }
+    */
 }
